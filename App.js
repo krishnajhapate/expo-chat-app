9126a0edf2873be362e02d9c67f890334aa4e373
krishnajhapate <ki54.jhapate@gmail.com>
2021-08-29 00:46:27 +0530

    removed npm to expo

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
+import { NavigationContainer,createStackNavigator } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Stack = createStackNavigator()
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+    </NavigationContainer>
   );
 }
+export default  App;
 
 const styles = StyleSheet.create({
   container: {
