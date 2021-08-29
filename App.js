737d845b4b038063cfc65839b0870853ea08e031
krishnajhapate <ki54.jhapate@gmail.com>
2021-08-29 13:03:23 +0530

    added screen setup and login screen

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import { NavigationContainer,createStackNavigator } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet} from 'react-native';
+import LoginScreen from './screens/login';
 
-const Stack = createStackNavigator()
+const Stack = createNativeStackNavigator()
 
-function App() {
+export default function App() {
   return (
     <NavigationContainer>
-
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-export default  App;
 
 const styles = StyleSheet.create({
   container: {
--- screens/login.jsx
+++ screens/login.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import { Input } from 'react-native-elements/dist/input/Input'
 
 export default function LoginScreen() {
+
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+            <Input
+                placeholder="Enter your email"
+
+            />
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
