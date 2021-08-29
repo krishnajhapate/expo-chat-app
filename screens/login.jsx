3c751ce8cf39517ca24f04049f274933bb2158ab
krishnajhapate <ki54.jhapate@gmail.com>
2021-08-29 13:46:08 +0530

    login screen built

--- screens/login.jsx
+++ screens/login.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 import { Input } from 'react-native-elements/dist/input/Input'
+import { Button } from 'react-native-elements'
+import { color } from 'react-native-elements/dist/helpers'
 
 export default function LoginScreen() {
 
@@ -11,8 +14,29 @@ export default function LoginScreen() {
         <View style={styles.container}>
             <Input
                 placeholder="Enter your email"
+                onChangeText={value => setEmail(value)}
+                label="Email"
+                leftIcon={{ type: 'font-awesome', name: 'envelope', color: 'gray', size: 20 }}
+                labelStyle={styles.label}
+                placeholderTextColor="gray"
+                leftIconContainerStyle={styles.leftIcon}
+            // errorMessage="ERROR"
+            // errorStyle={{ color: 'red' }}
+
 
             />
+            <Input
+                placeholder="Enter you password"
+                secureTextEntry
+                label="Password"
+                leftIcon={{ type: 'font-awesome', name: 'lock', color: 'gray', size: 25 }}
+                labelStyle={styles.label}
+                leftIconContainerStyle={styles.leftIcon}
+                onChangeText={value=>setPassword(value)}
+
+            />
+            <Button title="Sign in" raised />
+            <Button title="Sign up" raised containerStyle={styles.btn} />
         </View>
     )
 }
@@ -20,6 +44,19 @@ export default function LoginScreen() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        padding: 10,
+        marginTop: 30,
+        paddingLeft: 30,
+        paddingRight: 30,
+    },
+    label: {
+        fontSize: 11, color: 'rgba(0,0,0,.7)'
+    },
+    leftIcon: {
+        paddingRight: 10,
+    }
+    ,btn:{
+        marginTop:12,
     }
 })
